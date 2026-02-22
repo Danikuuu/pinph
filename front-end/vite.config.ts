4e5f7341a8b1b50609ac7429193ae845a9b2d0d6
Danikuuu <danikuroque@gmail.com>
2026-02-23 00:06:37 +0800

    api for server

--- front-end/vite.config.ts
+++ front-end/vite.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://pinpoint-nrae.onrender.com',
         changeOrigin: true,
       },
     },
