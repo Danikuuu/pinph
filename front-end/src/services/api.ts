04f78e2719bc80cf6899f0d673fdaa8286a9eb20
Danikuuu <danikuroque@gmail.com>
2026-02-23 00:22:19 +0800

    getting the server api from the .env file

--- front-end/src/services/api.ts
+++ front-end/src/services/api.ts
@@ -2,9 +2,9 @@ import axios from 'axios';
 import type { User, Note, UserProfile, UpdateProfileData, ApiResponse, PaginatedNotes } from '../types';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: import.meta.env.VITE_API_URL + '/api',
   headers: { 'Content-Type': 'application/json' },
-});
+})
 
 // Attach token automatically
 api.interceptors.request.use((config) => {
